7c9b10a07b3afe47769efcbf6b6800f941c60b61
take7spring <take7springtk@gmail.com>
2024-04-07 11:20:50 +0900

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+import '@fortawesome/fontawesome-free'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(fas, far, fab)
